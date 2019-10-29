2c225c2ce4c9d5a6e256933f55cb569b37d4a148
gregleeper <gregleeper19@gmail.com>
2019-10-29 09:55:47 -0500

    initial commit

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,68 +1,143 @@
-import bcrypt from 'bcryptjs'
-import getUserId from '../utils/getUserId'
-import generateToken from '../utils/generateToken'
-import hashPassword from '../utils/hashPassword'
+import bcrypt from "bcryptjs";
+import getUserId from "../utils/getUserId";
+import generateToken from "../utils/generateToken";
+import hashPassword from "../utils/hashPassword";
 
 const Mutation = {
-    async createUser(parent, args, { prisma }, info) {
-        const password = await hashPassword(args.data.password)
-        const user = await prisma.mutation.createUser({
-            data: {
-                ...args.data,
-                password
-            }
-        })
+  async createUser(parent, args, { prisma }, info) {
+    const password = await hashPassword(args.data.password);
+    const user = await prisma.mutation.createUser({
+      data: {
+        ...args.data,
+        password
+      }
+    });
 
-        return {
-            user,
-            token: generateToken(user.id)
-        }
-    },
-    async login(parent, args, { prisma }, info) {
-        const user = await prisma.query.user({
-            where: {
-                email: args.data.email
-            }
-        })
+    return {
+      user,
+      token: generateToken(user.id)
+    };
+  },
+  async login(parent, args, { prisma }, info) {
+    const user = await prisma.query.user({
+      where: {
+        email: args.data.email
+      }
+    });
 
-        if (!user) {
-            throw new Error('Unable to login')
-        }
-
-        const isMatch = await bcrypt.compare(args.data.password, user.password)
-
-        if (!isMatch) {
-            throw new Error('Unable to login')
-        }
-
-        return {
-            user,
-            token: generateToken(user.id)
-        }
-    },
-    async deleteUser(parent, args, { prisma, request }, info) {
-        const userId = getUserId(request)
-
-        return prisma.mutation.deleteUser({
-            where: {
-                id: userId
-            }
-        }, info)
-    },
-    async updateUser(parent, args, { prisma, request }, info) {
-        const userId = getUserId(request)
-
-        if (typeof args.data.password === 'string') {
-            args.data.password = await hashPassword(args.data.password)
-        }
-
-        return prisma.mutation.updateUser({
-            where: {
-                id: userId
-            },
-            data: args.data
-        }, info)
+    if (!user) {
+      throw new Error("Unable to login");
     }
-}
 
-export { Mutation as default }
+    const isMatch = await bcrypt.compare(args.data.password, user.password);
+
+    if (!isMatch) {
+      throw new Error("Unable to login");
+    }
+
+    return {
+      user,
+      token: generateToken(user.id)
+    };
+  },
+  async deleteUser(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+
+    return prisma.mutation.deleteUser(
+      {
+        where: {
+          id: userId
+        }
+      },
+      info
+    );
+  },
+  async updateUser(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+
+    if (typeof args.data.password === "string") {
+      args.data.password = await hashPassword(args.data.password);
+    }
+
+    return prisma.mutation.updateUser(
+      {
+        where: {
+          id: userId
+        },
+        data: args.data
+      },
+      info
+    );
+  },
+  async createCategory(parent, args, { prisma, request }, info) {
+    return prisma.mutation.createCategory({ data: args.data }, info);
+  },
+  async deleteCategory(parent, args, { prisma, request }, info) {
+    return prisma.mutation.deleteCategory(
+      {
+        where: {
+          id: args.where.id
+        }
+      },
+      info
+    );
+  },
+  async updateCategory(parent, args, { prisma, request }, info) {
+    return prisma.mutation.updateCategory(
+      {
+        where: {
+          id: args.where.id
+        },
+        data: args.data
+      },
+      info
+    );
+  },
+  async createRecipe(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+    return await prisma.mutation.createRecipe(
+      { data: { ...args.data, author: { connect: { id: userId } } } },
+      info
+    );
+  },
+  deleteRecipe(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+    const recipeExists = prisma.exists.Recipe({
+      id: args.where.id,
+      author: {
+        id: userId
+      }
+    });
+    if (recipeExists) {
+      return prisma.mutation.deleteRecipe(
+        { where: { id: args.where.id } },
+        info
+      );
+    }
+    throw new Error(
+      "Recipe does not exist or you must be the author to delete the recipe."
+    );
+  },
+  updateRecipe(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+    const recipeExists = prisma.exists.Recipe({
+      id: args.where.id,
+      author: { id: userId }
+    });
+
+    if (recipeExists) {
+      return prisma.mutation.updateRecipe(
+        {
+          where: { id: args.where.id },
+          data: args.data
+        },
+        info
+      );
+    }
+    throw new Error(
+      "Recipe does not exist or you must be the author to update."
+    );
+  }
+};
+
+export { Mutation as default };
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,33 +1,95 @@
-import getUserId from '../utils/getUserId'
+import getUserId from "../utils/getUserId";
 
 const Query = {
-    users(parent, args, { prisma }, info) {
-        const opArgs = {
-            first: args.first,
-            skip: args.skip,
-            after: args.after,
-            orderBy: args.orderBy
-        }
-        
-        if (args.query) {
-            opArgs.where = {
-                OR: [{
-                    name_contains: args.query
-                }]
-            }
-        }
+  users(parent, args, { prisma }, info) {
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy
+    };
 
-        return prisma.query.users(opArgs, info)
-    },
-    me(parent, args, { prisma, request }, info) {
-        const userId = getUserId(request)
-        
-        return prisma.query.user({
-            where: {
-                id: userId
-            }
-        })
+    if (args.query) {
+      opArgs.where = {
+        OR: [
+          {
+            name_contains: args.query
+          }
+        ]
+      };
     }
-}
 
-export { Query as default }
+    return prisma.query.users(opArgs, info);
+  },
+  me(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+
+    return prisma.query.user({
+      where: {
+        id: userId
+      }
+    });
+  },
+  categories(parent, args, { prisma, request }, info) {
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy
+    };
+
+    if (args.query) {
+      opArgs.where = {
+        OR: [
+          {
+            name_contains: args.query
+          }
+        ]
+      };
+    }
+
+    return prisma.query.categories(opArgs, info);
+  },
+  category(parent, args, { prisma, request }, info) {
+    return prisma.query.category(
+      {
+        where: {
+          id: args.where.id
+        }
+      },
+      info
+    );
+  },
+  recipes(parent, args, { prisma, request }, info) {
+    const opArgs = {
+      first: args.first,
+      skip: args.skip,
+      after: args.after,
+      orderBy: args.orderBy
+    };
+
+    if (args.query) {
+      opArgs.where = {
+        OR: [
+          {
+            title_contains: args.query
+          }
+        ]
+      };
+    }
+
+    return prisma.query.recipes(opArgs, info);
+  },
+  recipe(parent, args, { prisma, request }, info) {
+    return prisma.query.recipe(
+      {
+        where: {
+          id: args.where.id
+        }
+      },
+      info
+    );
+  }
+};
+
+export { Query as default };
